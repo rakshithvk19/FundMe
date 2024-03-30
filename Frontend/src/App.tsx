443f83b7e41390122426f782beb860ff744bcbb6
rakshitvk19 <rakshithvk19@gmail.com>
2024-03-30 15:17:53 -0400

    Basic layout

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import OwnerModal from "./components/OwnerModal";
+import FundersModal from "./components/FundersModal";
 
 function App() {
   return (
-    <div>
-      <Button variant="contained">Hello world</Button>
-    </div>
+    <main>
+      <Container maxWidth="sm">
+        <Paper sx={{ border: 1, borderColor: "primary.main", height: "100vh" }}>
+          <OwnerModal />
+          <FundersModal />
+        </Paper>
+      </Container>
+    </main>
   );
 }
 
---
+++ Frontend/src/components/OwnerModal.tsx
@@ -0,0 +1,33 @@
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+function OwnerModal() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+    console.log("Form submitted");
+  };
+
+  return (
+    <Paper sx={{ padding: "0.5em" }}>
+      <form onSubmit={handleSubmit}>
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1em",
+            alignItems: "center",
+          }}
+        >
+          <TextField label="Name" variant="outlined" />
+          <Button type="submit" variant="contained">
+            Fund
+          </Button>
+        </Box>
+      </form>
+    </Paper>
+  );
+}
+
+export default OwnerModal;
